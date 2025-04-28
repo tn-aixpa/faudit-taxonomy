d75d54d9a7f1c6a2f224288ed25862df609c2332
Davide Vandelli <113171147+FluveFV@users.noreply.github.com>
2025-04-28 16:22:22 +0200

    Add files via upload

--- SuggerimentiAziende.py
+++ SuggerimentiAziende.py
@@ -8,7 +8,7 @@ from fuzzywuzzy import fuzz, process
 # Titolo (Tempo di esecuzione in una CPU locale)
     # specifica
 #######################################
-# Preparazione dati delle organizzazioni (112ms)
+# Preparation dati Aziendali (112ms)
 #si deve rimuovere una piccola parte dei dati che era originalmente chiamata in modo 
 # simile a quella dei comuni, 'ID_tassonomia', seppur avendo una diversa tassonomia 
 
@@ -42,115 +42,20 @@ fp = 'outputOrganizzazioni/'
 os.makedirs(fp, exist_ok=True)
 
 #######################################
-# Azioni di basso utilizzo considerato l'intero dataset (8ms)
+#  List of actions to eliminate (low frequency)  (8ms)
 bassa_frequenza = tassonomia.sort_values(by='Frequenza della combinazione nell\'intero dataset')
 soglia = input('Mostrare le azioni utilizzate meno di __ volte:')
-bassa_frequenza[bassa_frequenza['Frequenza della combinazione nell\'intero dataset'] <= int(soglia)].to_csv(f'{fp}bassa_frequenza.csv')
-print(f'Osservazioni con meno di {soglia} righe salvate in {fp}bassa_frequenza.csv')
+bassa_frequenza[bassa_frequenza['Frequenza della combinazione nell\'intero dataset'] <= int(soglia)].to_csv(f'{fp}azioni_aziende_eliminare.csv')
+print(f'Osservazioni con meno di {soglia} righe salvate in {fp}azioni_aziende_eliminare.csv')
 print(bassa_frequenza.head())
 
     #Si può ovviamente invertire la logica e selezionare tutte le azioni con frequenza 
     # maggiore di (>=) una data soglia. O semplicemente ordinare la visualizzazione delle
     # azioni dal maggiore al minore. 
 
-#######################################
-# Azioni ordinate per utilizzo di UNA organizzazione (57ms)
-
-# e.g. '12345055' o '5055'
-print('L\'ID dell\'organizzazione si trova più facilmente se si specificano le ultime 4 cifre dell\'identificativo.')
-user_input = input("ID dell'organizzazione da analizzare (inserire l\'ID specifico di una organizzazione coinvolta):") 
-choices = piani_aziendali.ID_organizzazione.astype(str).unique()  
-
-azienda = process.extractOne(str(user_input.strip()), choices, scorer=fuzz.ratio)[0]
-focus = piani_aziendali[piani_aziendali.ID_organizzazione==azienda]
-
-rank = focus.value_counts(subset=['Label 0', 'codice_macro', 'descrizione_codice_macro', 'numero_codice_campo', 'descrizione_codice_campo']).reset_index().rename(columns=
-                                                                            {'Label 0':'ID_tassonomia', 
-                                                                             'count':'Frequenza nell\'organizzazione'})
-
-rank = pd.merge(rank, tassonomia[['ID_tassonomia', 
-                                  'Frequenza della combinazione nell\'intero dataset', 
-                                  'Numero di organizzazioni che ne hanno fatto uso nel dataset']], how='left')
-rank.to_csv(f'{fp}{azienda}.csv')
-print(f'Le frequenze di azioni {fp}{azienda}.csv')
-print(rank.head())
-
-#######################################
-# Serie storica per una lista di voci della tassonomia selezionate dall'utente (125ms)
-    # e.g. di input precompilato:
-# user_inputs = [11630769,
-#                11630764,
-#                11630751,
-#                11630770]               
-
-user_inputs = []
-
-while True:
-    entry = input("Azione da visualizzare (scrivi 'END' per terminare la lista): ")
-    if entry.strip() == "END":
-        break
-    user_inputs.append(entry.strip())
-
-print("Voci inserite:", user_inputs)
-    
-
-choices = tassonomia.ID_tassonomia
-
-    # identificazione degli input tra le scelte possibili
-if len(user_inputs) > 1:
-    azione = [process.extract(str(a), choices)[0][0] for a in user_inputs ]
-elif len(user_inputs): 
-    azione = [process.extract(str(user_inputs[0]), choices)[0][0]]
-
-    # preparazione dati per la visualizzazione
-def wrapper(text, words_per_line=3):    
-    words = text.replace('/',' - ').split()
-    lines_per_chunk = words_per_line%len(words) + 1  
-    lines = [' '.join(words[i:i+words_per_line]) for i in range(0, len(words), words_per_line)]    
-    chunks = ['<br>'.join(lines[i:i+lines_per_chunk]) for i in range(0, len(lines), lines_per_chunk)]    
-    return chunks[0]
-    
-storia = pd.DataFrame(columns=['anno_compilazione'])
-for a in azione:
-    t = piani_aziendali[piani_aziendali['Label 0'] == a]
-    #si deve "rompere" a causa della frequente lunghezza eccessiva
-    try:
-        a = wrapper(a)
-    except:
-        a
-    t = t.anno_compilazione.value_counts().reset_index().rename(columns={'count':a})
-    
-    if storia.shape[0]==0:
-        storia = t
-    else:
-        storia = pd.merge(storia, t, on='anno_compilazione', how='left')    
-storia = storia.fillna(0).sort_values(by='anno_compilazione')
-storia.to_csv(f'{fp}serie_storica.csv')
-print(f'Serie storica per le azioni selezionate salvata in {fp}serie_storica.csv')
-print(storia.head())
-print()
-    # visualizzazione 
-import plotly.express as px
-
-df_long = storia.melt(id_vars='anno_compilazione', value_vars=storia,
-                 var_name='Voce della tassonomia', value_name='Value')
-fig = px.line(
-    df_long,
-    x='anno_compilazione',
-    y='Value',
-    color='Voce della tassonomia',
-    markers=True,
-)
-    
-fig.update_layout(
-    xaxis_title="Annualità",
-    yaxis_title="Frequenza di utilizzo"
-)
-fig.write_html(f"{fp}serie_storica_azione.html")
-print(f'Visualizzazione interattiva salvata in {fp}serie_storica_azione.html')
 
 ########################################
-# Azioni simili ma categorizzate con diverse voci  (2m 45.61s)
+# List of actions to merge (2m 45.61s)
 # (suggerendo possibili errori, confusioni o incompletezze nella compilazione dell'organizzazione che 
 # fa uso della tassonomia)
 
@@ -233,6 +138,7 @@ for c in tqdm(piani_aziendali.ID_organizzazione.unique()):
                 match = process.extractOne(res[0], view[view['Label 0'].isin(l_a)].descrizione.values, scorer=fuzz.token_set_ratio)[0]
                 second = view[ (view['Label 0'].isin(l_a)) & (view.descrizione==match)]['Label 0'].values[0]
                 ID_tassonomia_2.append(second)
+                
 azioni_diversimili = pd.DataFrame({'ID_tassonomia_1':ID_tassonomia_1,
                      'descrizione_1':Azione_1,
                       'ID_tassonomia_2':ID_tassonomia_2,
@@ -250,6 +156,15 @@ summary = (
 )
 
 summary = summary.rename(columns={'count':'Frequenza'})
-summary.sort_values(by='Frequenza',ascending=False).to_csv(f'{fp}azioni_diversimili.csv')
-print(f'Tabella di azioni somiglianti ma che hanno diverse voci nella tassonomia salvata in {fp}azioni_diversimili.csv')
+summary.sort_values(by='Frequenza',ascending=False).to_csv(f'{fp}azioni_da_unire.csv')
+print(f'Tabella di azioni somiglianti ma che hanno diverse voci nella tassonomia salvata in {fp}azioni_da_unire.csv')
 print(summary.head())
+
+########################################
+# List of actions to "create", or actually split into smaller categories due to high frequency of use
+
+bassa_frequenza = tassonomia.sort_values(by='Frequenza della combinazione nell\'intero dataset')
+soglia = input('Mostrare le azioni utilizzate più di __ volte:')
+bassa_frequenza[bassa_frequenza['Frequenza della combinazione nell\'intero dataset'] >= int(soglia)].to_csv(f'{fp}azioni_aziende_dividere.csv')
+print(f'Osservazioni con più di {soglia} righe salvate in {fp}azioni_aziende_dividere.csv')
+print(bassa_frequenza.head())
