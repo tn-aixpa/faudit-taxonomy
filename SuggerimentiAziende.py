2ed23a4029494c1327936366e81d04776171904f
Davide Vandelli <113171147+FluveFV@users.noreply.github.com>
2025-05-07 10:59:45 +0200

    Add files via upload

--- SuggerimentiAziende.py
+++ SuggerimentiAziende.py
@@ -12,21 +12,9 @@ from fuzzywuzzy import fuzz, process
 #si deve rimuovere una piccola parte dei dati che era originalmente chiamata in modo 
 # simile a quella dei comuni, 'ID_tassonomia', seppur avendo una diversa tassonomia 
 
-piani_aziendali = pd.read_parquet('data/piani_aziendali.gzip').dropna(subset='Label 0').drop(columns='ID_tassonomia')  
-piani_aziendali['Label 0'] = piani_aziendali['Label 0'].astype(int)
-
-tassonomia = piani_aziendali[['Label 0','codice_macro', 
-                 'descrizione_codice_macro', 'numero_codice_campo',
-                'descrizione_codice_campo']].value_counts().reset_index().rename(columns={'Label 0':'ID_tassonomia'}
-                                                                                ).sort_values(by='ID_tassonomia'
-                                                                                             ).rename(columns={'count':'Frequenza della combinazione nell\'intero dataset'})
-
-c = []
-for i in tassonomia[['ID_tassonomia', 'codice_macro', 'numero_codice_campo']].values:    
-    view = piani_aziendali[(piani_aziendali['Label 0'] == i[0]) & (piani_aziendali['codice_macro'] == i[1]) & (piani_aziendali['numero_codice_campo'] == i[2])]
-    c.append(view.ID_organizzazione.unique().shape[0])
-tassonomia['Numero di organizzazioni che ne hanno fatto uso nel dataset'] = c
-
+piani_aziendali = pd.read_parquet('data/piani_aziendali.gzip')
+tassonomia = pd.read_csv('data/tassonomia_aziende.csv', sep=';')
+tassonomia = pd.merge(piani_aziendali['Label 0'].value_counts().reset_index().rename(columns={'count':'Frequenza delle azioni', 'Label 0':'ID_tassonomia'}), tassonomia)
 # I dati delle organizzazioni (aziende) sono più complessi di quelli comunali. 
 # Ogni osservazione può avere più di una categoria nella tassonomia del Family Audit, 
 # e può avere più di un codice macro e più di un codice campo. 
@@ -43,19 +31,18 @@ os.makedirs(fp, exist_ok=True)
 
 #######################################
 #  List of actions to eliminate (low frequency)  (8ms)
-bassa_frequenza = tassonomia.sort_values(by='Frequenza della combinazione nell\'intero dataset')
 soglia = input('Mostrare le azioni utilizzate meno di __ volte:')
-bassa_frequenza[bassa_frequenza['Frequenza della combinazione nell\'intero dataset'] <= int(soglia)].to_csv(f'{fp}azioni_aziende_eliminare.csv')
-print(f'Osservazioni con meno di {soglia} righe salvate in {fp}azioni_aziende_eliminare.csv')
+bassa_frequenza = tassonomia[tassonomia['Frequenza di utilizzo dell\'azione'] <= int(soglia)]
+bassa_frequenza.to_csv(f'{fp}azioni_eliminare.csv', sep=';', index=False)
+print(f'Osservazioni con meno di {soglia} righe salvate in {fp}azioni_eliminare.csv')
 print(bassa_frequenza.head())
 
     #Si può ovviamente invertire la logica e selezionare tutte le azioni con frequenza 
     # maggiore di (>=) una data soglia. O semplicemente ordinare la visualizzazione delle
     # azioni dal maggiore al minore. 
 
-
 ########################################
-# List of actions to merge (2m 45.61s)
+# List of actions to merge (21)
 # (suggerendo possibili errori, confusioni o incompletezze nella compilazione dell'organizzazione che 
 # fa uso della tassonomia)
 
@@ -75,64 +62,75 @@ ID_tassonomia_2 = []
 Azione_1 = []
 Azione_2 = []
 Levenshtein = []
-for c in tqdm(piani_aziendali.ID_organizzazione.unique()):
+aziende = list(piani_aziendali.ID_organizzazione.unique())#[:10] #D
+for c in tqdm(aziende):
     view = piani_aziendali[piani_aziendali.ID_organizzazione == c]
 
-    l_a = view['Label 0'].unique().tolist() #insieme di azioni pubblicate dall'organizzazione    
+    l_a = view['Label 0'].fillna(0).drop_duplicates().tolist() #insieme di categorie delle azioni pubblicate dall'organizzazione    
     
-    seen = []    
-
     for i in l_a:
         for j in l_a:        
             if i == j:
                 continue            
-            first = l_a.pop(0)
+            first = l_a.pop(0) # si parte dal primo elemento dell'insieme vs tutto il rimanente
             #print(l_a)
-            a = view[view['Label 0'] == first].descrizione.fillna(0).values            
-            choices = view[view['Label 0'].isin(l_a)].descrizione.fillna(0).values
-            choices = [' '.join(p.simple_tokenizer(ascificatore(option))) if option != 0 else option for option in choices]            
+            a = view[(view['Label 0'] == first) & (~view['Label 0'].isin(l_a))].descrizione.drop_duplicates().fillna(0).values
+            if a.shape[0] > 1: 
+                a = a.tolist()
+        
+            choices = view[view['Label 0'].isin(l_a)].descrizione.drop_duplicates().fillna(0).values
+            choices2 = [' '.join(p.simple_tokenizer(ascificatore(option))) for option in choices] 
             # Nei dati delle organizzazioni si nota che il titolo è frequentemente
             # soggetto a duplicazione per azioni ben diverse, fenomeno che avviene
             # notevolmente di meno nei dati dei Comuni. Di conseguenza, 
             # nel caso delle organizzazioni si usa la descrizione (tokenizzata)
             # per individuare azioni di diverse categorie implementate in maniera simile. 
-             
-            
-            # Scenario in cui ci sono multiple azioni sotto la stessa categoria in posizione j                          
-            if a.shape[0] > 1: 
+                         
+            # Scenario in cui ci sono multiple decsrizioni sotto la stessa categoria in posizione j                          
+            if len(a) > 1: 
                 
-                a = [' '.join(p.simple_tokenizer(ascificatore(option))) if option != 0 else option for option in a]
-                for azione in a:
-                    if azione == 0: #descrizione vuota, inutile paragonare 
+                a2 = [' '.join(p.simple_tokenizer(ascificatore(option))) if option != 0 else option for option in a]                 
+                
+                for azione in range(len(a2)):
+                    if a2[azione] == 0: #descrizione vuota, inutile paragonare 
                         continue
-                    res = process.extractOne(azione, choices, scorer=fuzz.token_sort_ratio)                    
+                        
+                    res = process.extractOne(a2[azione], choices2, scorer=fuzz.token_set_ratio)                                    
+                    
                     if res[1] < 75:  #se il risultato è scadente, si passa alla prossima iterazione
                         continue
                     
-                    ID_tassonomia_1.append(first)
-                    Azione_1.append(azione)
-                    Azione_2.append(res[0])
+                    ID_tassonomia_1.append(first)                    
+                    Azione_1.append(a[azione])
+                    
+                    idx = choices2.index(res[0]) #indice di dove si trova il miglior match nella lista così da ritirarlo nella versione originale                
+                    Azione_2.append(choices[idx])
+                                        
                     Levenshtein.append(res[1])  
-
+                    
                     # È fondamentale recuperare la categoria della seconda voce, 
                     # identificata come la più simile, ma process.extractOne fornisce la posizione. 
                     # Il recupero avviene qui sotto:
-                    match = process.extractOne(res[0], view[view['Label 0'].isin(l_a)].descrizione.values)[0]
+                    match = process.extractOne(res[0], view[view['Label 0'].isin(l_a)].descrizione.values, scorer=fuzz.token_set_ratio)[0]
                     second = view[ (view['Label 0'].isin(l_a)) & (view.descrizione==match)]['Label 0'].values[0]
                     ID_tassonomia_2.append(second)
             
-            # Scenario in cui c'è una sola azione sotto la categoria in posizione j
-            else: 
-                if a == 0: #descrizione vuota, inutile paragonare 
-                        continue
-                a = ' '.join(p.simple_tokenizer(ascificatore(a[0])))                
+            # Scenario in cui c'è una sola descrizione sotto la categoria in posizione j
+            elif len(a) == 1: 
+
+                a2 = ' '.join(p.simple_tokenizer(ascificatore(a[0])))                
                 
-                res = process.extractOne(a, choices, scorer=fuzz.token_sort_ratio)                    
+                res = process.extractOne(a2, choices2, scorer=fuzz.token_set_ratio)                    
+                
                 if res[1] < 75: 
                     continue
-                ID_tassonomia_1.append(first)
-                Azione_1.append(a)
-                Azione_2.append(res[0])
+                    
+                ID_tassonomia_1.append(first)                
+                Azione_1.append(a[0])
+
+                idx = choices2.index(res[0]) #indice di dove si trova il miglior match nella lista così da ritirarlo nella versione originale
+                
+                Azione_2.append(choices[idx])
                 Levenshtein.append(res[1])
                 
                 match = process.extractOne(res[0], view[view['Label 0'].isin(l_a)].descrizione.values, scorer=fuzz.token_set_ratio)[0]
@@ -140,31 +138,33 @@ for c in tqdm(piani_aziendali.ID_organizzazione.unique()):
                 ID_tassonomia_2.append(second)
                 
 azioni_diversimili = pd.DataFrame({'ID_tassonomia_1':ID_tassonomia_1,
-                     'descrizione_1':Azione_1,
-                      'ID_tassonomia_2':ID_tassonomia_2,
-                     'descrizione_2':Azione_2,
-                     'Levenshtein':Levenshtein})
+                                   'descrizione_1':Azione_1,
+                                   'ID_tassonomia_2':ID_tassonomia_2,
+                                   'descrizione_2':Azione_2,
+                                   'Levenshtein':Levenshtein})
+
 summary = (
     azioni_diversimili
     .groupby(['ID_tassonomia_1', 'ID_tassonomia_2'])
-    .agg(
-        count=('descrizione_1', 'count'),
-        titoli_1_list=('descrizione_1', lambda x: list(set(x))),
-        titoli_2_list=('descrizione_2', lambda x: list(set(x)))
+    .agg(count=('descrizione_1', 'count'),
+         descrizione_1_list=('descrizione_1', lambda x: x if isinstance(x, str) else ' | '.join(x).replace('\n', ' ')),         
+         descrizione_2_list=('descrizione_2', lambda x: x if isinstance(x, str) else ' | '.join(x).replace('\n', ' ')),         
+         Percentuale_di_similarità=('Levenshtein', lambda x: x)
     )
     .reset_index()
 )
 
+
 summary = summary.rename(columns={'count':'Frequenza'})
-summary.sort_values(by='Frequenza',ascending=False).to_csv(f'{fp}azioni_da_unire.csv')
-print(f'Tabella di azioni somiglianti ma che hanno diverse voci nella tassonomia salvata in {fp}azioni_da_unire.csv')
+summary.sort_values(by='Frequenza',ascending=False).to_csv(f'{fp}azioni_unire.csv', sep=';', index=False)
+print(f'Tabella di azioni somiglianti ma che hanno diverse voci nella tassonomia salvata in {fp}azioni_unire.csv')
 print(summary.head())
 
 ########################################
 # List of actions to "create", or actually split into smaller categories due to high frequency of use
 
-bassa_frequenza = tassonomia.sort_values(by='Frequenza della combinazione nell\'intero dataset')
+bassa_frequenza = tassonomia[tassonomia['Frequenza di utilizzo dell\'azione'] >= int(soglia)]
 soglia = input('Mostrare le azioni utilizzate più di __ volte:')
-bassa_frequenza[bassa_frequenza['Frequenza della combinazione nell\'intero dataset'] >= int(soglia)].to_csv(f'{fp}azioni_aziende_dividere.csv')
-print(f'Osservazioni con più di {soglia} righe salvate in {fp}azioni_aziende_dividere.csv')
+bassa_frequenza[bassa_frequenza['Frequenza di utilizzo dell\'azione'] >= int(soglia)].to_csv(f'{fp}azioni_dividere.csv', sep=';', index=False)
+print(f'Osservazioni con meno di {soglia} righe salvate in {fp}azioni_dividere.csv')
 print(bassa_frequenza.head())
--- SuggerimentiComuni.py
+++ SuggerimentiComuni.py
@@ -10,29 +10,21 @@ from fuzzywuzzy import fuzz, process
 
 #######################################
 # Preparation dati Comunali (472ms)
-istat = pd.read_csv('data/codici_istat.csv', sep=';', encoding='Windows-1252')
-base = pd.read_excel('data/tassonomia_comuni.xlsx').rename(columns={'Azione':'azione'})
 piani_comunali = pd.read_parquet('data/piani_comunali.gzip')
+tassonomia = pd.read_csv('data/tassonomia_comuni.csv', sep=';')
+tassonomia = pd.merge(piani_comunali.ID_tassonomia.value_counts().reset_index().rename(columns={'count':'Frequenza delle azioni'}), tassonomia)
 
-tassonomia = pd.merge(base,
-                      piani_comunali[['ID_tassonomia', 'azione', 'codice_macro', 
-                                      'descrizione_codice_macro', 'numero_codice_campo', 
-                                      'descrizione_codice_campo']].value_counts().reset_index().sort_values(by='ID_tassonomia'),
-                     on = 'azione', 
-                     how= 'right')
-    
 fp = 'outputComuni/'
 os.makedirs(fp, exist_ok=True)
 
 #######################################
 # List of actions to eliminate (low frequency) (8ms)
-view = tassonomia.sort_values(by='count').rename(columns={'count':'Frequenza'})
 # asking: Select an upper threshold for the frequencies of rarely used actions 
-soglia = input('Soglia superiore per la frequenza di azioni raramente usate:')
+soglia = input('Mostrare le azioni utilizzate meno di __ volte:')
 
-bassa_frequenza = view[view['Frequenza'] <= int(soglia)]
-bassa_frequenza.to_csv(f'{fp}azioni_comuni_eliminare.csv')
-print(f'Azioni con meno di {soglia} osservazioni salvate in {fp}azioni_comuni_eliminare.csv')
+bassa_frequenza = tassonomia[tassonomia['Frequenza delle azioni'] <= int(soglia)]
+bassa_frequenza.to_csv(f'{fp}azioni_eliminare.csv', sep=';', index=False)
+print(f'Azioni con meno di {soglia} osservazioni salvate in {fp}azioni_eliminare.csv')
 print(bassa_frequenza.head())
 
 ########################################
@@ -60,33 +52,34 @@ for c in tqdm(piani_comunali.comune_breve.unique()):
     view = piani_comunali[piani_comunali.comune_breve == c]
 
     l_a = view.ID_tassonomia.unique().tolist() #insieme di azioni pubblicate dal comune    
-    
-    seen = []    
-
+        
     for i in l_a:
         for j in l_a:        
             if i == j:
                 continue            
             first = l_a.pop(0)
             #print(l_a)
-            a = view[view.ID_tassonomia == first].titolo.values
-            choices = view[view.ID_tassonomia.isin(l_a)].titolo.values
-            choices = [' '.join(p.simple_tokenizer(ascificatore(option))) for option in choices]            
+            a = view[view.ID_tassonomia == first].titolo.fillna(0).drop_duplicates().values
+            choices = view[view.ID_tassonomia.isin(l_a)].titolo.fillna(0).drop_duplicates().values
+            choices2 = [' '.join(p.simple_tokenizer(ascificatore(option))) for option in choices]            
             
             # Scenario in cui ci sono multiple azioni sotto la stessa categoria in posizione j              
             if a.shape[0] > 1: 
-                a = [' '.join(p.simple_tokenizer(ascificatore(option))) for option in a]
-                for azione in a:                    
-                    if azione == 'nan':
+                a2 = [' '.join(p.simple_tokenizer(ascificatore(option))) for option in a]
+                for azione in range(len(a2)):                    
+                    if azione == 0:
                         continue
                     
-                    res = process.extractOne(azione, choices, scorer=fuzz.token_sort_ratio)                    
+                    res = process.extractOne(a2[azione], choices2, scorer=fuzz.token_sort_ratio)                    
                     if res[1] < 75:  #se il risultato è scadente, si passa alla prossima iterazione
                         continue
                     
                     ID_tassonomia_1.append(first)
-                    Azione_1.append(azione)
-                    Azione_2.append(res[0])
+                    Azione_1.append(a2[azione])
+                    
+                    idx = choices2.index(res[0]) #indice di dove si trova il miglior match nella lista così da ritirarlo nella versione originale                
+                    Azione_2.append(choices[idx])
+                                        
                     Levenshtein.append(res[1])  
 
                     # È fondamentale recuperare la categoria della seconda voce, 
@@ -97,17 +90,16 @@ for c in tqdm(piani_comunali.comune_breve.unique()):
                     ID_tassonomia_2.append(second)
                                 
             # Scenario in cui c'è una sola azione sotto la categoria in posizione j
-            else: 
-                a = ' '.join(p.simple_tokenizer(ascificatore(a[0])))                
-                if a == 'nan':
-                    continue
+            elif len(a) == 1:
+                a2 = ' '.join(p.simple_tokenizer(ascificatore(a[0])))                
                     
-                res = process.extractOne(a, choices, scorer=fuzz.token_sort_ratio)                    
+                res = process.extractOne(a2, choices2, scorer=fuzz.token_sort_ratio)                    
                 if res[1] < 75: 
                     continue
                 ID_tassonomia_1.append(first)
-                Azione_1.append(a)
-                Azione_2.append(res[0])
+                Azione_1.append(a[0])
+                idx = choices2.index(res[0]) #indice di dove si trova il miglior match nella lista così da ritirarlo nella versione originale                
+                Azione_2.append(choices[idx])
                 Levenshtein.append(res[1])
 
                 match = process.extractOne(res[0], view[view.ID_tassonomia.isin(l_a)].titolo.values)[0]
@@ -121,26 +113,24 @@ azioni_diversimili = pd.DataFrame({'ID_tassonomia_1':ID_tassonomia_1,
 summary = (
     azioni_diversimili
     .groupby(['ID_tassonomia_1', 'ID_tassonomia_2'])
-    .agg(
-        count=('titolo_1', 'count'),
-        titoli_1_list=('titolo_1', lambda x: list(set(x))),
-        titoli_2_list=('titolo_2', lambda x: list(set(x)))
+    .agg(count=('titolo_1', 'count'),
+         titoli_1_list=('titolo_1', lambda x: x if isinstance(x, str) else ' | '.join(x).replace('\n', ' ') ),
+         titoli_2_list=('titolo_2', lambda x: x if isinstance(x, str) else ' | '.join(x).replace('\n', ' ') ),
+         Percentuale_di_similarità=('Levenshtein', lambda x: list(x))    
     )
     .reset_index()
 )
 
 summary = summary.rename(columns={'count':'Frequenza'})
-summary.sort_values(by='Frequenza',ascending=False).to_csv(f'{fp}azioni_simili_unire.csv')
-print(f'Le azioni, somiglianti per descrizione ma con voci diverse della tassonomia, sono salvate in {fp}azioni_simili_unire.csv')
+summary.sort_values(by='Frequenza',ascending=False).to_csv(f'{fp}azioni_unire.csv', sep=';', index=False)
+print(f'Azioni somiglianti ma che hanno diverse voci nella tassonomia salvata in {fp}azioni_unire.csv')
 print(summary.head())
 
 ########################################
 # List of actions to "create", or actually split into smaller categories due to high frequency of use
-view = tassonomia.sort_values(by='count').rename(columns={'count':'Frequenza'})
 # asking: Select a lower threshold for the frequencies of rarely used actions 
-soglia = input('Soglia inferiore per la frequenza di azioni usate spesso:')
-
-bassa_frequenza = view[view['Frequenza'] >= int(soglia)]
-bassa_frequenza.to_csv(f'{fp}azioni_da_dividere.csv')
-print(f'Azioni con più di {soglia} osservazioni salvate in {fp}azioni_da_dividere.csv')
+soglia = input('Mostrare le azioni utilizzate più di __ volte:')
+bassa_frequenza = tassonomia[tassonomia['Frequenza delle azioni'] >= int(soglia)]
+bassa_frequenza.to_csv(f'{fp}azioni_da_dividere.csv', sep=';', index=False)
+print(f'Azioni con più di {soglia} osservazioni salvate in {fp}azioni_dividere.csv')
 print(bassa_frequenza.head())
